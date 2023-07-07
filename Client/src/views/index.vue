dbdbcfe6db9070c537dcfbf61436d045ece747a0
范晓豪 <fanxiaohaohaha@qq.com>
2023-07-07 17:58:07 +0800

    客户端服务器连接成功

--- Client/src/views/index.vue
+++ Client/src/views/index.vue
@@ -40,18 +40,49 @@
       <!--        <div class="tools">-->
       <!--          <div>emoji</div>-->
       <!--        </div>-->
-      <textarea class="chat-input" placeholder="pleaseEnterContent"/>
+      <textarea class="chat-input" placeholder="pleaseEnterContent" v-model="text" @keydown.enter="toMsg"/>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {inject} from "vue";
-import MessageList from "@/components/messageList/index.vue";
-import UserList from "@/components/userList/index.vue";
+// socket
+import {inject, onMounted, onUnmounted, ref} from "vue"
+import MessageList from "@/components/messageList/index.vue"
+import UserList from "@/components/userList/index.vue"
+import {io, Socket} from 'socket.io-client'
 // global
-const state = inject<any>("state");
-console.log(state.message)
+const state = inject<any>("state")
+console.log(state)
+const text = ref('');
+const socket = ref<Socket | null>(null)
+
+const toMsg = () => {
+  if (text.value) {
+    socket.value?.emit('chat message', text.value)
+    text.value = ''
+  }
+}
+
+onMounted(() => {
+  socket.value = io('http://localhost:33445')
+
+  socket.value.on('connect', () => {
+    console.log('Connected to the server')
+  })
+
+  socket.value.on('chat message', (msg: string) => {
+    console.log('Received a message: ' + msg)
+  })
+
+  socket.value.emit('chat message', 'Hello Server!')
+})
+
+onUnmounted(() => {
+  if (socket.value) {
+    socket.value.disconnect()
+  }
+})
 
 </script>
 
--- Server/index.js
+++ Server/index.js
@@ -1,24 +1,46 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const socketIO = require('socket.io');
+const Docenv = require('dotenv')
+Docenv.config();
 
+const PORT = process.env.PORT
+
+// 创建一个新的 express 应用
 const app = express();
+
+// 创建一个新的 http 服务器，使用 express 应用
 const server = http.createServer(app);
-const io = socketIo(server);
+const options = {
+  cors: {
+    origin: 'http://127.0.0.1:5173',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true
+  }
+}
+// 创建一个新的 Socket.IO 实例，使用 http 服务器
+const io = socketIO(server, options);
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+// 当有新的客户端连接时，执行以下回调函数
+io.on('connection', function (socket) {
+  console.log('A user connected: ' + socket.id);
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+  // 当客户端发送 "chat message" 事件时，执行以下回调函数
+  socket.on('chat message', function (msg) {
+    console.log('message: ' + msg);
 
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
-    });
+    // 将 "chat message" 事件广播给所有客户端，包括发送这个事件的客户端
+    io.emit('chat message', msg);
+  });
+
+  // 当客户端断开连接时，执行以下回调函数
+  socket.on('disconnect', function () {
+    console.log('A user disconnected: ' + socket.id);
+  });
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+// 启动服务器，监听 3000 端口
+server.listen(PORT, function () {
+  console.log(`Server is listening on *:${PORT}`);
 });
