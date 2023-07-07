e17bac26bae0fd9b0f227ddd4f676004ff6dcb1d
范晓豪 <fanxiaohaohaha@qq.com>
2023-07-07 10:56:15 +0800

    fix

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+// import dayjs from 'dayjs'
+// // 中文本地化插件
+// import 'dayjs/locale/zh-cn'
+// dayjs.locale('zh-cn')
 
 createApp(App).mount('#app')
