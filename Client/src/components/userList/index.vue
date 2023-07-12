d12de13daf3f54942b73e9991818df8bd59db653
范晓豪 <fanxiaohaohaha@qq.com>
2023-07-12 12:53:09 +0800

    build

--- Client/src/components/userList/index.vue
+++ Client/src/components/userList/index.vue
@@ -14,6 +14,7 @@
 <script setup lang="ts">
 import {computed, defineComponent, onMounted, ref} from 'vue'
 import {time} from "@/utils";
+import {User} from "@/views/type.ts";
 
 const currentTime = ref(new Date().getTime());
 
@@ -33,7 +34,7 @@ defineComponent({
 
 defineProps({
   UserListData: {
-    type: Array,
+    type: Array as () => User[],
     default: () => []
   }
 })
