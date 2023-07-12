e6df478e1a483d5b6e7ece3a7f4fd11b08393257
范晓豪 <fanxiaohaohaha@qq.com>
2023-07-12 08:53:14 +0800

    跨域问题

--- Server/index.js
+++ Server/index.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 // 创建一个新的 express 应用
 const app = express();
-const allowedOrigin = ["http://82.157.118.166", "http://127.0.0.1:20007", "http://localhost:20007"];
+const allowedOrigin = ["http://82.157.118.166:20007", "http://127.0.0.1:20007", "http://localhost:20007"];
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", allowedOrigin); // 允许所有来源的请求
