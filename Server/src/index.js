b4f601d9d135359b3044f77653f63cdd4a41ba15
sumingcheng <cjwdyzdyplxjg@gmail.com>
2023-11-13 17:46:05 +0800

    fix: 代码优化

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,76 +1,65 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const {findUserName, replaceUser} = require('./utils');
+const express = require('express')
+const http = require('http')
+const socketIO = require('socket.io')
+const {findUserName, replaceUser} = require('./utils')
 
-let BASE_URL, PORT
-if (process.env.NODE_ENV === 'production') {
-  PORT = 33445
-  BASE_URL = '0.0.0.0'
-} else {
-  PORT = 33445
-  BASE_URL = "127.0.0.1"
-}
-console.log("当前环境为", process.env.NODE_ENV)
+const PORT = process.env.NODE_ENV === 'production' ? 33445 : 33445
+const BASE_URL = process.env.NODE_ENV === 'production' ? '0.0.0.0' : '127.0.0.1'
+console.log('当前环境为', process.env.NODE_ENV)
 
-const app = express();
-const allowedOrigin = ["http://82.157.118.166:20007", "http://127.0.0.1:20007", "http://localhost:20007"];
+const app = express()
+const allowedOrigins = ['http://82.157.118.166:20007', 'http://127.0.0.1:20007', 'http://localhost:20007']
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", allowedOrigin);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  next();
-});
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', allowedOrigins)
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
+  next()
+})
 
-const server = http.createServer(app);
-const options = {
+const server = http.createServer(app)
+const ioOptions = {
   cors: {
-    origin: allowedOrigin,
+    origin: allowedOrigins,
     methods: ['GET', 'POST'],
     allowedHeaders: ['my-custom-header'],
     credentials: true
   }
 }
 
-const io = socketIO(server, options);
-// 创建在线用户列表
-let onlineUsers = [];
+const io = socketIO(server, ioOptions)
+let onlineUsers = []
 
-io.on('connection', function (socket) {
-  const username = socket.handshake.query.username;
-  let newUser = {
-    username: username,
-    id: socket.id
-  }
-  // 如果用户名已经存在于在线用户列表中，则断开这个连接
+io.on('connection', (socket) => {
+  const username = socket.handshake.query.username
+  const newUser = {username, id: socket.id}
+
   if (findUserName(onlineUsers, username)) {
     replaceUser(onlineUsers, newUser)
   } else {
-    onlineUsers.push(newUser);
+    onlineUsers.push(newUser)
   }
 
-  console.log(`用户『${username}』已连接: ${socket.id}`);
-  io.emit('onlineUsers', onlineUsers);
+  console.log(`用户『${username}』已连接: ${socket.id}`)
+  io.emit('onlineUsers', onlineUsers)
 
-  socket.on('ServerMessage', function (msgObj) {
-    msgObj.timestamp = Date.now();
-    msgObj.id = socket.id;
-    msgObj.username = username;
-    io.emit('Client', msgObj);
-  });
+  socket.on('ServerMessage', (msgObj) => {
+    const message = {
+      ...msgObj,
+      timestamp: Date.now(),
+      id: socket.id,
+      username
+    }
+    io.emit('Client', message)
+  })
 
-  socket.on('disconnect', function () {
-    console.log(`用户${username}已断开连接：${socket.id}`);
+  socket.on('disconnect', () => {
+    console.log(`用户${username}已断开连接：${socket.id}`)
+    onlineUsers = onlineUsers.filter(user => user.id !== socket.id)
+    io.emit('onlineUsers', onlineUsers)
+  })
+})
 
-    // 从在线用户列表中移除断开连接的用户
-    onlineUsers = onlineUsers.filter(user => user.id !== socket.id);
-
-    // 将更新后的在线用户列表发送给所有连接的客户端
-    io.emit('onlineUsers', onlineUsers);
-  });
-});
-
-server.listen(PORT, `${BASE_URL}`, function () {
-  console.log(`服务运行在 http://${BASE_URL}:${PORT} 端口`);
-});
+server.listen(PORT, BASE_URL, () => {
+  console.log(`服务运行在 http://${BASE_URL}:${PORT} 端口`)
+})
--- Server/src/utils/index.js
+++ Server/src/utils/index.js
@@ -1,17 +1,17 @@
 // 查找用户名
 function findUserName(arr, username) {
-  let user = arr.find((item) => item.username === username);
-  return user ? user.username : null;
+  let user = arr.find((item) => item.username === username)
+  return user ? user.username : null
 }
 
 function replaceUser(array, newUser) {
   for (let i = 0; i < array.length; i++) {
     if (array[i].username === newUser.username) {
-      array[i] = newUser;
-      break;
+      array[i] = newUser
+      break
     }
   }
-  return array;
+  return array
 }
 
 module.exports = {
